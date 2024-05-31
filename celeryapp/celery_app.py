53cdb603890c80c921d15eb2dd40449814738983
Hangukin <korea5643@naver.com>
2024-06-01 01:04:56 +0900

    test

--- celeryapp/celery_app.py
+++ celeryapp/celery_app.py
@@ -25,7 +25,7 @@ celery_app.conf.update(
 celery_app.conf.beat_schedule = {
     'price_processing': {
         'task': 'src.task.tasks.preprocessing_price',
-        'schedule': crontab(hour=01,minute=5),
+        'schedule': crontab(hour=1,minute=6),
         'args':()
     }
 }
