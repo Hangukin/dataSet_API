cce110db2e45fd15c311a8f17e81cc36efa751b8
hangukin <korea5643@naver.com>
2024-07-24 15:20:28 +0900

    dataDB commit

--- src/db/dataDB.py
+++ src/db/dataDB.py
@@ -88,8 +88,9 @@ async def db_select_hotelID(query):
 
 async def db_select_hw_dail_price(query):
     table = 'HW_LDGS_DAIL_MAX_AVRG_MIN_PRC_INFO'
+    
     if query.last_id == None:
-        total_count = prisma.table.count(where={'LDGMNT_DE':query.ldgmnt_de})
+        total_count = prisma.hw_ldgs_dail_max_avrg_min_prc_info.count(where={'LDGMNT_DE':query.ldgmnt_de})
         data = await prisma.query_raw(
             f"SELECT * FROM {table} use index (primary) WHERE LDGMNT_DE = {query.ldgmnt_de} ORDER BY id ASC LIMIT 3000;"
             )
