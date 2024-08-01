d50825cdf7b3de149c39fa87eaca9a734f553abb
hangukin <korea5643@naver.com>
2024-08-01 13:59:24 +0900

    dataDB commit

--- src/db/dataDB.py
+++ src/db/dataDB.py
@@ -93,21 +93,18 @@ async def db_select_hw_dail_price(query):
     table = 'HW_LDGS_DAIL_MAX_AVRG_MIN_PRC_INFO'
     if query.last_id == None: # 첫 페이지 호출 시 
         total_count = await prisma.query_raw(
-            f"SELECT count(*) as total_rows FROM {table} use index(LDGMNT_DE) WHERE LDGMNT_DE = {query.ldgmnt_de}"
+            f"SELECT count(*) as total_rows FROM {table} use index(LDGMNT_DE) WHERE {table}.LDGMNT_DE = '{query.ldgmnt_de}'"
         )
-        if int(query.ldgmnt_de) > 20240531:
-            last_id = 62472773
-        else:
-            last_id = 1
-        
+      
         data = await prisma.query_raw(
-            f"SELECT * FROM {table} use index (primary) WHERE LDGMNT_DE = {query.ldgmnt_de} AND id > {last_id} ORDER BY id ASC LIMIT 5000;"
+            f"SELECT * FROM {table} use index (LDGMNT_DE) WHERE {table}.LDGMNT_DE = '{query.ldgmnt_de}' ORDER BY id ASC LIMIT 20000;"
             )
         
         result = {'total_count':total_count[0]['total_rows'],'last_id':data[-1]['id'],'result':data}
+        
     else:
         data = await prisma.query_raw(
-            f"SELECT * FROM {table} use index (primary) WHERE LDGMNT_DE = '{query.ldgmnt_de}' AND id > {query.last_id} ORDER BY id ASC LIMIT 5000;"
+            f"SELECT * FROM {table} use index (primary) WHERE {table}.LDGMNT_DE = '{query.ldgmnt_de}' AND id > {query.last_id} ORDER BY id ASC LIMIT 20000;"
             )
         
         result = {'last_id':data[-1]['id'], 'result':data}
